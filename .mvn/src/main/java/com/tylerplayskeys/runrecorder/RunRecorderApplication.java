1dfb56ad1e883c3435a28de71845bac310b0a0ee
Tyler Acosta <tacos1998@gmail.com>
2024-06-28 14:00:29 -0500

    add src

---
+++ .mvn/src/main/java/com/tylerplayskeys/runrecorder/RunRecorderApplication.java
@@ -0,0 +1,13 @@
+package com.tylerplayskeys.runrecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunRecorderApplication.class, args);
+	}
+
+}
---
+++ .mvn/src/test/java/com/tylerplayskeys/runrecorder/RunRecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tylerplayskeys.runrecorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunRecorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
